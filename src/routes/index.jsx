9990e00ceb6861701515b0e3e48e6d08cf34c2ff
Pieter Leviano <pieterleviano@gmail.com>
2025-05-03 21:42:58 +0700

    [REFAC] added layouts

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import AuthLayout from "../layouts/AuthLayout.jsx";
+import StoreLayout from "../layouts/StoreLayout.jsx";
+import Login from '/src/Home/Login.jsx';
+
 import Register from '/src/Home/Register.jsx';
 import Home from '/src/Home/Home.jsx';
-import Login from '/src/Home/Login.jsx';
 import ProductHome from '/src/Products/ProductHome.jsx';
 import DetailProduct from '/src/Products/DetailProduct.jsx';
 import ProtectedRoute from './ProtectedRoutes.jsx';
@@ -13,11 +17,9 @@ const router = createBrowserRouter([
         element: <div>Routes Not Found!</div>
     },
     {
+        // Auth Layout -> Tidak ada navbar
+        element: <AuthLayout />,
         children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
             {
                 path: "/login",
                 element: (
@@ -26,23 +28,43 @@ const router = createBrowserRouter([
                     </RedirectIfLoggedIn>
                 ),
             },
+        ],
+    },
+    {
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+
+            // {
+            //     path: "/login",
+            //     element: (
+            //         <RedirectIfLoggedIn>
+            //             <Login />
+            //         </RedirectIfLoggedIn>
+            //     ),
+            // },
+
             {
                 path: "/register",
                 element: <Register />,
             },
+
             {
                 path: "/products",
                 element: (
                     // <ProtectedRoute allowedRoles={["owner", "admin", "gudang"]}>
-                        <ProductHome />
+                    <ProductHome />
                     // </ProtectedRoute>
                 ),
             },
+
             {
                 path: "/products/detail/:id",
                 element: (
                     // <ProtectedRoute allowedRoles={["owner", "admin", "gudang"]}>
-                        <DetailProduct />
+                    <DetailProduct />
                     // </ProtectedRoute>
                 ),
             },
